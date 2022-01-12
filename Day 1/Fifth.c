0a134bc54863faaa675a518996b8433ec4333352
Utku Halis <personal@utkuhalis.com>
2022-01-12 06:53:06 +0300

    .

---
+++ Day 1/Fifth.c
@@ -0,0 +1,12 @@
+main()
+{
+	for(int i=1; i<=10; i++){
+		for(int i2=1; i2<=10; i2++){
+			printf("%dx%d=%d	", i, i2, i*i2);
+		}
+		printf("\n");
+	}
+	
+
+    return 0;
+}
--- Day 1/Fourth.c
+++ Day 1/Fourth.c
@@ -1,1 +1,27 @@
+main()
+{
+	for(int i=0; i<=10; i++){
+		printf("i=%d \n", i);
+	}
+	
+	
+	printf("\n");
+	
+	int n = 0;
+	while(n <= 10){
+		printf("n=%d \n", n);
+		n++;
+	}
+	
+	printf("\n");
+	
+	int n2 = 0;
+	
+	do {
+		printf("n=%d \n", n2);
+		++n2;
+	}
+	while (n2 <= 10);
 
+    return 0;
+}
