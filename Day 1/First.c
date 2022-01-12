815ac94b776970c61e53e33c99a1f7107039429b
Utku Halis <personal@utkuhalis.com>
2022-01-12 06:34:04 +0300

    day1

---
+++ Day 1/First.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main()
+{
+    printf("%d \n", 100);
+    printf("%c \n", 65);
+    printf("%s \n", "Utku Halis");
+    printf("%f \n", 94.65);
+    printf("%% \n");
+}
