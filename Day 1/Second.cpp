e99636457ef801288591b3e494bfdc3cb626fa64
Utku Halis <personal@utkuhalis.com>
2022-01-12 06:37:14 +0300

    .

---
+++ Day 1/Second.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+main()
+{
+	int n = 100;
+	char c = 65;
+	char s[] = "Utku Halis";
+	float f = 94.65;
+	
+    printf("%d \n", n);
+    printf("%c \n", c);
+    printf("%s \n", s);
+    printf("%f \n", f);
+    printf("%% \n");
+    
+    return 0;
+}
